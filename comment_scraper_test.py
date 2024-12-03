5a42a8ae9ee8d4f91cc633613045dd3dbdc28fe7
Jackson Hanna <86638009+jsh8080@users.noreply.github.com>
2024-12-03 09:34:39 -0600

    Update comment_scraper_test.py
    added the code to put the comments on a separate text file for analysis. The problem remains that the code does not take all the comments.

--- comment_scraper_test.py
+++ comment_scraper_test.py
@@ -2,19 +2,18 @@ import googleapiclient.discovery
 
 youtube = googleapiclient.discovery.build("youtube","v3",developerKey="AIzaSyCVJjcD77f13zb2tHTsmFslvF8Dm9efn-Q")
 
-request = youtube.commentThreads().list(part="snippet",videoId="SIm2W9TtzR0",maxResults=100).execute()
+request = youtube.commentThreads().list(part="snippet",videoId="0BjlBnfHcHM",maxResults=100).execute()
 
 comments = []
+comment_storage = open('comment_base.txt','w',encoding='utf-8')
 count = 0
 
-#currently only displays some comments an not others, further refinement needed
-#https://www.youtube.com/watch?v=SIm2W9TtzR0 this is the video currently being scraped
+
 for item in request['items']:
     count += 1
     print(count)
     print(item['snippet']['topLevelComment']['snippet']['textDisplay'])
-    print()
-    print()
+    entry = item['snippet']['topLevelComment']['snippet']['textDisplay'] + '\n\n'
+    comment_storage.write(entry)
 
-
-                
+comment_storage.close()
