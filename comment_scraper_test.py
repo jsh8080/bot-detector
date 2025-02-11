6c74b30badf751cb7a76f3257dede4fcfe9e13b1
Jackson Hanna <86638009+jsh8080@users.noreply.github.com>
2025-02-11 09:42:03 -0600

    Update comment_scraper_test.py

--- comment_scraper_test.py
+++ comment_scraper_test.py
@@ -2,15 +2,16 @@ import googleapiclient.discovery
 
 youtube = googleapiclient.discovery.build("youtube","v3",developerKey="AIzaSyCVJjcD77f13zb2tHTsmFslvF8Dm9efn-Q")
 
-request = youtube.commentThreads().list(part="snippet",videoId="0BjlBnfHcHM",maxResults=100).execute()
+request = youtube.commentThreads().list(part="snippet",videoId="zrsPs3CzHWs",maxResults=500).execute()
 
 comments = []
-comment_storage = open('comment_base.txt','w',encoding='utf-8')
+comment_storage = open('botDetector\comment_base.txt','w',encoding='utf-8')
 count = 0
 
 
 for item in request['items']:
     count += 1
+    print("\n")
     print(count)
     print(item['snippet']['topLevelComment']['snippet']['textDisplay'])
     entry = item['snippet']['topLevelComment']['snippet']['textDisplay'] + '\n\n'
