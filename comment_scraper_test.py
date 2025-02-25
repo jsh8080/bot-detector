b6ca8b18f150e48c732e131ac7918dd34e299734
Jackson Hanna <86638009+jsh8080@users.noreply.github.com>
2025-02-25 09:34:09 -0600

    Update comment_scraper_test.py
    put the comment scraping process into a function so that it can be called else ware in order to consolidate code

--- comment_scraper_test.py
+++ comment_scraper_test.py
@@ -2,19 +2,22 @@ import googleapiclient.discovery
 
 youtube = googleapiclient.discovery.build("youtube","v3",developerKey="AIzaSyCVJjcD77f13zb2tHTsmFslvF8Dm9efn-Q")
 
-request = youtube.commentThreads().list(part="snippet",videoId="zrsPs3CzHWs",maxResults=500).execute()
-
 comments = []
-comment_storage = open('comment_base.txt','w',encoding='utf-8')
 count = 0
 
+def get_comments(Id):
+    request = youtube.commentThreads().list(part="snippet",videoId=Id,maxResults=500).execute()
+    comment_storage = open('botDetector\comment_base.txt','w',encoding='utf-8')
+    count = 0
+    for item in request['items']:
+        count += 1
+        print("\n")
+        print(count)
+        print(item['snippet']['topLevelComment']['snippet']['textDisplay'])
+        entry = item['snippet']['topLevelComment']['snippet']['textDisplay'] + '\n\n'
+        comment_storage.write(entry)
 
-for item in request['items']:
-    count += 1
-    print("\n")
-    print(count)
-    print(item['snippet']['topLevelComment']['snippet']['textDisplay'])
-    entry = item['snippet']['topLevelComment']['snippet']['textDisplay'] + '\n\n'
-    comment_storage.write(entry)
+    comment_storage.close()
 
-comment_storage.close()
+#use only to test out the function in this file, otherwise leave commented
+#get_comments()
